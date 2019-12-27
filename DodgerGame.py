8bfe829291c18f832ed7e949d32c0932aa19a5ec
Hrvoje <vhrvoje2@net.hr>
2019-12-27 21:36:27 +0100

    Added collision, item respawning after collision, score and health update on collision

--- DodgerGame.py
+++ DodgerGame.py
@@ -17,7 +17,8 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 #object class
 class Object:
-    def __init__(self, objectX, objectY, objectChange):
+    def __init__(self, isType, objectX, objectY, objectChange):
+        self.isType = isType
         self.objectX = objectX
         self.objectY = objectY
         self.objectChange = objectChange
@@ -35,28 +36,60 @@ def generateInt(letter):
     if letter == 'X':
         return random.randint(0, screenWidth-32)
     elif letter == 'Y':
-        return random.randint(0, screenHeight//20)
+        return random.randint(0, screenHeight//7)
     elif letter == 'C':
         return random.randint(1, 2)
 
-#player settings
-player = Object(screenWidth/2-32, screenHeight-64, 0)
-player.defineImage('player.png')
+def collision(item, player):
+    if item.isType == 'bomb':
+        itemYTemp = item.objectY + 32
+        for x in range(int(player.objectX), int(player.objectX)+65):
+            for y in range(item.objectX, item.objectX+9):
+                distance = math.sqrt((math.pow(y-x, 2)) + (math.pow(itemYTemp-player.objectY,2)))
+                if distance <= 1:
+                    return True
 
-#gift and enemy setting
+    else:            
+        itemYTemp = item.objectY + 30
+        for x in range(int(player.objectX), int(player.objectX)+65):
+            for y in range(item.objectX, item.objectX+29):
+                distance = math.sqrt((math.pow(y-x, 2)) + (math.pow(itemYTemp-player.objectY,2)))
+                if distance <= 1:
+                    return True
+    return False
+
+def gameOver(score):
+    print('Game over')
+    return 1
+
+def newItem():
+    objectList.remove(item)
+    objectList.append(spawnItem())
+    return
+
+def spawnItem():
+    if random.randint(1, 5) > 4:
+        gift = Object('gift', generateInt('X'), generateInt('Y'), generateInt('C'))
+        gift.defineImage('gift.png')
+        return gift
+    else:
+        bomb = Object('bomb', generateInt('X'), generateInt('Y'), generateInt('C'))
+        bomb.defineImage('bomb.png')
+        return bomb
+
+#player settings
+player = Object('player', screenWidth/2-32, screenHeight-42, 0)
+player.defineImage('player.png')
+playerSpeed = 3
+health = 3
+score = 0
+
+#gift and bomb setting
 objectList = []
 objectCount = 10
 
 for i in range(objectCount):
-
-    if random.randint(1, 5) > 4:
-        gift = Object(generateInt('X'), generateInt('Y'), generateInt('C'))
-        gift.defineImage('gift.png')
-        objectList.append(gift)
-    else:
-        enemy = Object(generateInt('X'), generateInt('Y'), generateInt('C'))
-        enemy.defineImage('enemy.png')
-        objectList.append(enemy)
+    objectList.append(spawnItem())
         
 #main loop
 running = True
@@ -64,29 +97,39 @@ running = True
 while running:
     screen.fill((255, 255, 255))
 
+    #keypresses
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player.objectChange -= 1
+                player.objectChange -= playerSpeed
             if event.key == pygame.K_RIGHT:
-                player.objectChange += 1
+                player.objectChange += playerSpeed
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.objectChange = 0
 
-
+    #move items
     for item in objectList:
         item.objectY += item.objectChange
         item.objectDraw()
 
-        if item.objectY >= screenHeight:
-            item.objectX = generateInt('X')
-            item.objectY = generateInt('Y')
-            item.objectChange = generateInt('C')
+        if collision(item, player):
+            if item.isType == 'bomb':
+                health -= 1
+                print(health)
+                if health == 0:
+                    gameOver(score)
+                newItem()
+            else:
+                score += 1
+                print(score)
+                newItem()
 
+        if item.objectY >= screenHeight:
+            newItem()
     player.objectMove()
     if player.objectX <= 0:
         player.objectX = 0
@@ -98,9 +141,5 @@ while running:
     pygame.display.update()
 
     #todo
-    #add collision
-    #add life points
-    #add score
-    #add background
-    #add music
-    #add sounds
+    #add life points display
+    #add score display
